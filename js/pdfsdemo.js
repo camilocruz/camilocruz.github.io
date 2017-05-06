d4841134639d7f69285615038019fc1e9039848f
camilocruz <camilo.cruz@gmail.com>
2017-05-06 00:21:29 -0500

    s

--- js/pdfsdemo.js
+++ js/pdfsdemo.js
@@ -1,5 +1,8 @@
 // If absolute URL from the remote server is provided, configure the CORS
 // header on that server.
+
+var x=document.getElementById('the-canvas');
+alert(x);
 var url = '../pdf/TFHKA_Platino V2.2.pdf';
 
 
